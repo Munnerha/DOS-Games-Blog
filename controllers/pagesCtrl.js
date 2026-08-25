e758b2606311cc5266ae69a652cb7c624f2b2de4
Muneer <85256095+Munnerha@users.noreply.github.com>
2026-08-25 16:35:02 +0300

    Third Day

--- controllers/pagesCtrl.js
+++ controllers/pagesCtrl.js
@@ -1,5 +1,24 @@
+const User = require('../models/user');
+
 const home = async (req, res) => {
-  res.render('index.ejs');
+  try {
+    // Find the admin user (who holds all posts)
+    const adminUser = await User.findOne({ role: 'admin' });
+    
+    // Get their posts, or empty array if no admin exists
+    let posts = [];
+    if (adminUser) {
+      posts = adminUser.posts;
+    }
+    
+    // Render the homepage and pass the posts
+    res.render('index.ejs', { posts });
+    
+  } catch (err) {
+    console.log(err);
+    // If error, render with empty posts
+    res.render('index.ejs', { posts: [] });
+  }
 };
 
 module.exports = {
